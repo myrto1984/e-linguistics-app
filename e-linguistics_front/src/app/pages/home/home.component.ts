5d3e527aeec637743de3403f9b2eff66104d57cc
jBalasis <cs2180008@di.uoa.gr>
2019-05-27 18:05:38 +0300

    more alignment

--- e-linguistics_front/src/app/pages/home/home.component.ts
+++ e-linguistics_front/src/app/pages/home/home.component.ts
@@ -3,36 +3,37 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 import {NltkService} from "../../services/nltk.service";
 
 @Component({
-    selector: 'app-home',
-    templateUrl: 'home.component.html'
+  selector: 'app-home',
+  templateUrl: 'home.component.html'
 })
 
-export class HomeComponent implements OnInit{
-    myForm: FormGroup;
-    result: string[];
-    perseus_lexicon_HTML: any;
+export class HomeComponent implements OnInit {
+  myForm: FormGroup;
+  result: string[];
+  perseus_lexicon_HTML: any;
 
-    constructor(private nltkService: NltkService,
-                private fb: FormBuilder) {}
+  constructor(private nltkService: NltkService,
+              private fb: FormBuilder) {
+  }
 
-    ngOnInit(): void {
-        this.initForm();
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.myForm = this.fb.group({input_text: ['']});
+  }
+
+  sendTextToBack() {
+    if (this.myForm.valid) {
+      this.nltkService.tokenizeText(this.myForm.value).subscribe(
+        res => this.result = res,
+        er => console.log(er)
+      );
     }
+  }
 
-    initForm() {
-        this.myForm = this.fb.group({ input_text: [''] });
-    }
-
-    sendTextToBack() {
-        if (this.myForm.valid) {
-            this.nltkService.tokenizeText(this.myForm.value).subscribe(
-                res => this.result = res,
-                er => console.log(er)
-            );
-        }
-    }
-
-    getLemma(word: string) {
-        this.perseus_lexicon_HTML = `http://www.perseus.tufts.edu/hopper/morph?l=${word}&la=gr`;
-    }
+  getLemma(word: string) {
+    this.perseus_lexicon_HTML = `http://www.perseus.tufts.edu/hopper/morph?l=${word}&la=gr`;
+  }
 }
