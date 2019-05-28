93ef38cf65fef3abc50910b65139319ab99ad371
myrto1984 <myrtokoukouli@gmail.com>
2019-05-28 17:31:30 +0300

    moved e-linguistics_data in the static folder

--- e-linguistics_back/src/config.py
+++ e-linguistics_back/src/config.py
@@ -1,6 +1,6 @@
-MONGO_URL =  # MONGO SERVER ADDRESS
-MONGO_PORT =  # PORT THE MONGO IS RUNNING ON
+MONGO_URL = 'mongo.server.address'    # MONGO SERVER ADDRESS
+MONGO_PORT = 'mongo_port'                  # PORT THE MONGO IS RUNNING ON
 
-DB_NAME = 'e_linguistics_db'        # THE NAME OF THE DB
-INSCR_COL = 'inscriptions'          # COLLECTIONS NAMES
+DB_NAME = 'db_name'        # THE NAME OF THE DB
+INSCR_COL = 'inscriptions' # COLLECTIONS NAMES
 WORDS_COL = 'grc_wordnet'
