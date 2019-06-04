37e3e447c90955d25d84ce16ccb110841bfcaa8c
jBalasis <cs2180008@di.uoa.gr>
2019-06-04 16:19:51 +0300

    some css added, some ui changes

--- e-linguistics_front/src/app/pages/home/home.component.ts
+++ e-linguistics_front/src/app/pages/home/home.component.ts
@@ -6,7 +6,8 @@ import {zip} from "rxjs";
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.component.html'
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.css']
 })
 
 export class HomeComponent implements OnInit {
